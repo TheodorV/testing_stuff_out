c8985cde3ae53b6b7de589b398191a0fba4cb804
TheodorV <valeanu.theodor@gmail.com>
2020-03-09 09:20:33 -0400

    terraform builder

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = var.AWS_REGION
+  access_key = "AKIAVRXPHV2ZBCVTOCZT"
+  secret_key = "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"
+}
+
---
+++ terraform/vars.tf
@@ -0,0 +1,36 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "/home/jenkins/.ssh/mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "/home/jenkins/.ssh/mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-03a1a94b7707461d3"
+  }
+}
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+
+variable "JENKINS_VERSION" {
+  default = "2.204.1"
+}
+
+variable "TERRAFORM_VERSION" {
+  default = "0.12.18"
+}
+
+variable "APP_INSTANCE_COUNT" {
+  default = "1"
+}
